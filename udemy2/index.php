5acb65d033b888a175ce518dbcf7f306c7f4a79f
John-Urquhart <surrey_dentist@hotmail.co.uk>
2024-03-26 14:32:13 +0000

    create Model class

--- udemy2/index.php
+++ udemy2/index.php
@@ -20,8 +20,8 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <body>
     <h1>Products</h1>
     <?php foreach ($products as $product): ?>
-    <h2><?= $product["name"]?></h2>
-    <p><?= $product["description"]?></p>
+    <h2><?= htmlspecialchars($product["name"])?></h2>
+    <p><?= htmlspecialchars($product["description"])?></p>
     <?php endforeach; ?>
 </body>
 </html>
