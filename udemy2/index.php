f38df85b8545b374b2d6c6c0f1e7e29a7340079f
John-Urquhart <surrey_dentist@hotmail.co.uk>
2024-03-27 10:25:45 +0000

    match function
    route paths

--- udemy2/index.php
+++ udemy2/index.php
@@ -4,7 +4,15 @@ $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 $segments = explode("/", $path);
 
+require "src/router.php";
 
+$router = new Router;
+
+$router->add("/home/index", ["controller" => "home", "action" => "index"]);
+$router->add("/products", ["controller" => "products", "action" => "index"]);
+$router->add("/", ["controller" => "home", "action" => "index"]);
+
+$router->match($path);
 
 $action = $segments[5];
 $controller = $segments[4];
--- udemy2/src/router.php
+++ udemy2/src/router.php
@@ -1,2 +1,22 @@
 <?php
 
+class router{
+    private array $routes = [];
+
+    public function add(string $path, array $params): void {
+
+        $this->routes[] =[
+            "path" => $path,
+            "params" => $params
+        ];
+    }
+
+    public function match(string $path): array|bool{
+        foreach($this->routes as $route){
+            if ($route["path"] === $path){
+                return $route["params"];
+            }
+        }
+        return false;
+    }
+}
